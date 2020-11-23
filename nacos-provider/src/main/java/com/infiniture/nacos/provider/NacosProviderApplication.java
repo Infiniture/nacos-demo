491058df5bf167544adb2ba2f935fc05d901fdb5
Infiniture <dunfang@mail.ustc.edu.cn>
2020-11-23 17:59:44 +0800

    init the project

---
+++ nacos-provider/src/main/java/com/infiniture/nacos/provider/NacosProviderApplication.java
@@ -0,0 +1,13 @@
+package com.infiniture.nacosprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosProviderApplication.class, args);
+	}
+
+}
