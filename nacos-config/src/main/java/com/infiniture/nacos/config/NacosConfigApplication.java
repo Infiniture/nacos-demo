a8319c49a577ba36d2d29a645bea4b88e48b10f8
Infiniture <dunfang@mail.ustc.edu.cn>
2020-11-24 19:36:54 +0800

    spring cloud项目接入nacos作为配置中心

--- nacos-config/src/main/java/com/infiniture/nacos/config/NacosConfigApplication.java
+++ nacos-config/src/main/java/com/infiniture/nacos/config/NacosConfigApplication.java
@@ -1,22 +1,28 @@
-package com.infiniture.nacos.provider;
+package com.infiniture.nacos.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 @EnableDiscoveryClient
 @SpringBootApplication
-public class NacosProviderApplication {
+public class NacosConfigApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NacosProviderApplication.class, args);
+		SpringApplication.run(NacosConfigApplication.class, args);
 	}
 
-	@GetMapping("/helloNacos")
-	public String helloNacos() {
-		return "你好，nacos！";
+	@Value("${nacos.config}")
+	private String config;
+
+	@GetMapping("/getValue")
+	public String getValue() {
+		return config;
 	}
 }
