835204bd2a76fb00dd92af95f3d9c52aaf35e032
Infiniture <dunfang@mail.ustc.edu.cn>
2020-11-24 15:32:20 +0800

    spring cloud项目接入nacos作为注册中心

--- nacos-consumer/src/main/java/com/infiniture/nacos/consumer/NacosConsumerApplication.java
+++ nacos-consumer/src/main/java/com/infiniture/nacos/consumer/NacosConsumerApplication.java
@@ -1,8 +1,17 @@
 package com.infiniture.nacos.consumer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NacosConsumerApplication {
 
@@ -10,4 +19,17 @@ public class NacosConsumerApplication {
 		SpringApplication.run(NacosConsumerApplication.class, args);
 	}
 
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@GetMapping("/consumer")
+	public String test() {
+		return restTemplate.getForObject("http://nacos-provider/helloNacos", String.class);
+	}
 }
--- nacos-provider/src/main/java/com/infiniture/nacos/provider/NacosProviderApplication.java
+++ nacos-provider/src/main/java/com/infiniture/nacos/provider/NacosProviderApplication.java
@@ -1,13 +1,23 @@
-package com.infiniture.nacosprovider;
+package com.infiniture.nacos.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@EnableDiscoveryClient
 @SpringBootApplication
 public class NacosProviderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NacosProviderApplication.class, args);
+	}
+
+	@GetMapping("/helloNacos")
+	public String helloNacos() {
+		return "你好，nacos！";
 	}
 
 }
